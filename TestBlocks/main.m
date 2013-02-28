fd08c4e58931248643426aaed4ea15c49984db2a
Malte Tancred <malte@tancred.com>
2013-02-28 22:46:55 +0100

    Initial Commit

---
+++ TestBlocks/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TestBlocks
+//
+//  Created by Malte Tancred on 2013-02-28.
+//  Copyright (c) 2013 Tancred. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
